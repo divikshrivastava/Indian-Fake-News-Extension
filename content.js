08410642dcf17d96abaa04eb6a6c04dd7b2272e8
Divik Shrivastava (SFE/DEL) <divik.s@safe.security>
2022-12-18 19:45:31 +0530

    update

---
+++ content.js
@@ -0,0 +1,6 @@
+console.log("HERE")
+var images = document.getElementsById('id__cl44082sjhc');
+// for (var i = 0, l = images.length; i < l; i++) {
+//   images[i].src = 'http://placekitten.com/' + images[i].width + '/' + images[i].height;
+// }
+console.log("ELE", images)
